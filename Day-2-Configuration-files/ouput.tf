3178c73c784c5d38ecac4334998178d9d3b3510c
yjiwankar-code <yjiwankar@gmail.com>
2025-11-06 20:43:17 +0530

    Day-2-configuration-files

---
+++ Day-2-Configuration-files/ouput.tf
@@ -0,0 +1,9 @@
+output "publicip" {
+  value = aws_instance.name.public_ip
+}
+output "privateip" {
+  value = aws_instance.name.private_ip
+}
+output "az" {
+  value = aws_instance.name.availability_zone
+}
---
+++ Day-2-Configuration-files/variables.tf
@@ -0,0 +1,12 @@
+variable "ami_id" {
+  description = "passing ami value"
+  default = ""
+  type = string
+}
+
+variable "type" {
+  description = "passing values to instance type"
+  default = ""
+  type = string 
+}
+
