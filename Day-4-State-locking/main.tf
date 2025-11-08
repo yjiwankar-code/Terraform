c6c09a418307f8cd591ad2bcf0c9764523a46f65
yjiwankar-code <yjiwankar@gmail.com>
2025-11-08 15:26:31 +0530

    Day-4

--- Day-4-State-locking/main.tf
+++ Day-4-State-locking/main.tf
@@ -1,14 +1,6 @@
 resource "aws_vpc" "name" {
   cidr_block = "10.0.0.0/16"
   tags = {
-    Name="Test-VPC"
-  }
-}
-
-resource "aws_subnet" "name" {
-  cidr_block = "10.0.0.0/24"
-  vpc_id = aws_vpc.name.vpc_id
-  tags = {
-    Name="Test-Subnet"
+    Name="Dev-VPC"
   }
 }
