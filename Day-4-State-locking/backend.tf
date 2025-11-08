bcd70f60efffd4b2c8112e7bb537b7c0f316f20a
yjiwankar-code <yjiwankar@gmail.com>
2025-11-08 16:33:56 +0530

    Day-4-DynamoDB-State-lock

--- Day-4-State-locking/backend.tf
+++ Day-4-State-locking/backend.tf
@@ -2,7 +2,9 @@ terraform {
   backend "s3" {
     bucket = "yjiwankars3bucket"
     key = "Day-4/terraform.tfstate"
-    use_lockfile = true
+    //use_lockfile = true
     region = "us-east-1"
+    dynamodb_table = "yogesh"
+    encrypt = true
   }
 }
