a1636f56446f75d928d0251f82e0da0b5f77f98d
yjiwankar-code <yjiwankar@gmail.com>
2026-01-28 16:59:54 +0530

    Day 04 state lock apply

--- Day-04-State-locking/backend.tf
+++ Day-04-State-locking/backend.tf
@@ -2,6 +2,7 @@ terraform {
   backend "s3" {
     bucket = "yjiwankar-test-bucket-1234567890-7709798512"
     key = "Day-04-State-locking/terraform-tfstate"
+    use_lockfile = true
     region = "us-east-1"
   }
 }
