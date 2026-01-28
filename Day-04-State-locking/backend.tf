5d5441065451df6bf9a9e54ac64249b23beba972
yjiwankar-code <yjiwankar@gmail.com>
2026-01-28 19:27:35 +0530

    added dynamodb lock in backend file

--- Day-04-State-locking/backend.tf
+++ Day-04-State-locking/backend.tf
@@ -2,7 +2,9 @@ terraform {
   backend "s3" {
     bucket = "yjiwankar-test-bucket-1234567890-7709798512"
     key = "Day-04-State-locking/terraform-tfstate"
-    use_lockfile = true
+    #use_lockfile = true  # to se s3 native locking version 1.19 and above
     region = "us-east-1"
+    dynamodb_table = "yogesh" # for any version we can use dynamodb table for locking
+    encrypt = true
   }
 }
